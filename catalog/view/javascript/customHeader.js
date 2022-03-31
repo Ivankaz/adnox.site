3ef189397ef1db943996d00ac900c2a9ad52d2c3
Ivan Kazakov <ivankaz9@yandex.ru>
2022-03-31 10:13:32 +0400

    Сделал открытие вложенных меню при наведении на ссылку

--- catalog/view/javascript/customHeader.js
+++ catalog/view/javascript/customHeader.js
@@ -1,22 +1,55 @@
 // функции шапки сайта
 function initCustomHeader() {
-  console.log('init');
-  let menu1_height = $('.customHeader__menu_1').outerHeight();
+  // обработчик события изменения активной ссылки меню
+  window.eventUpdatedActiveLink = function(activeLink = null) {
+    let submenuId = $(activeLink).data('submenu-id');
+    if (typeof submenuId == 'undefined') return false;
+    let submenuIdPrefix = submenuId.slice(0, submenuId.lastIndexOf("_")+1);
+    let submenu = $('.customHeader [data-submenu='+submenuId+']').get(0);
+    let submenuParent = $(submenu).parent();
+
+    // скрываю все вложенные меню
+    $(submenuParent).find('.js-submenu[data-submenu^='+submenuIdPrefix+']').addClass('h-hide');
+    // показываю родительский блок, если он был скрыт
+    $(submenuParent).removeClass('h-hide');
+    // показываю вложенное меню для активной ссылки
+    $(submenu).removeClass('h-hide');
+  }
 
   $(window).scroll(function() {
-    // если страницу прокрутили дальше высоты первого уровня меню
-    if ($(this).scrollTop() > menu1_height) {
+    // если страницу прокрутили
+    if ($(this).scrollTop() > 0) {
       // то скрываю 2 и 3 уровни меню
-      $('.customHeader__menu_2, .customHeader__menu_3').addClass('h-hide');
+      $('.customHeader__menu_2, .customHeader__menu_3, .customHeader .js-submenu').addClass('h-hide');
       // изменяю отступы у верхнего меню
       $('.customHeader__menu_1').addClass('menu1_scroll');
       // перемещаю кнопки в меню верхнего уровня
       $('.customHeader .menu__buttons').detach().appendTo('.customHeader__menu_1');
     } else {
-      $('.customHeader__menu_2').removeClass('h-hide');
+      $('.customHeader__menu_2, .customHeader__menu_2 .js-submenu:first').removeClass('h-hide');
       $('.customHeader__menu_1').removeClass('menu1_scroll');
       $('.customHeader .menu__buttons').detach().appendTo('.customHeader__menu_2');
     }
+  });
+
+  // если навели мышку на ссылку в меню 1 уровня
+  $('.customHeader__menu_1 .menu1__link').mouseover(function() {
+    // то устанавливаю эту ссылку активной
+    $('.customHeader__menu_1 .menu1__link').removeClass('menu1__link_active');
+    $(this).addClass('menu1__link_active');
+    // скрываю меню 3 уровня
+    $('.customHeader__menu_3').addClass('h-hide');
+    // обновляю меню 2 уровня
+    eventUpdatedActiveLink(this);
+  });
+
+  // если навели мышку на ссылку в меню 2 уровня
+  $('.customHeader__menu_2 .menu2__link').mouseover(function() {
+    // то устанавливаю эту ссылку активной
+    $('.customHeader__menu_2 .menu2__link').removeClass('menu2__link_active');
+    $(this).addClass('menu2__link_active');
+    // обновляю меню 3 уровня
+    eventUpdatedActiveLink(this);
   });
 }
 
